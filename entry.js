c4f857e8cbddce67c9db881fd19cd0164e80a29b
KidiIT <146373396+KidiIT@users.noreply.github.com>
2023-11-15 10:51:18 -0600

    CYBER-SECURITY ENTRY-LEVEL JOBS APPLICATION FORM.
    this project will give you a in-depth knowledge on how form validation with "JAVASCRIPT" works. however, the form validation project has a comprehensive template to use to get your form validated on your own terms with HTML,CSS,JAVASCRIPT.

---
+++ entry.js
@@ -0,0 +1,48 @@
+let entryForm = document.forms.def
+let usersInput = document.querySelectorAll("input")
+entryForm.onsubmit=(e)=>{
+    e.preventDefault()
+ let  formErr= {
+    usernameErr: '',
+    passwordErr: '',
+    aspiringErr: '',
+    contactErr: '',
+
+    }
+    let username = entryForm.username.value.trim();
+    let password = entryForm.password.value.trim();
+    let aspiring = entryForm.aspiring.value.trim();
+    let contact = entryForm.contact.value.trim();
+    if(username == ''){
+        formErr.usernameErr= 'please fill in the blank';
+    }else if(username.length < 8 || username.includes("[A-Z]")){
+        formErr.usernameErr= 'must start with a uppercase[A-Z] with more than 8 characters';
+
+    }else if(password == ''){
+        formErr.passwordErr='please fill in the blank';
+
+    }else if(password.length < 8 || password.includes("@$#%*!")){
+        formErr.passwordErr='must be more than 8 characters with any of the special characters["@$#%*!]';
+
+    }else if(aspiring == ''){
+        formErr.aspiringErr='invalid password';
+
+    }else if(contact == ''){
+        formErr.contactErr = 'this field is requried';
+
+    }else{
+       console.log({username,password,aspiring});
+       alert("Congratulation you've successfully submitted your application,Thanks for time your "+username);
+
+    }
+    let errElement = document.querySelectorAll('.err')
+    console.log(errElement)
+    for(let i=0; i < errElement.length; i++){
+        let errElements = errElement[i];
+
+        console.log(formErr);
+        errElements.innerHTML = formErr[""+errElements.dataset.err+""]
+
+    }
+
+}
