5da023948d7e655a37749c6c26c66f8173a3063c
KIDI'S-TECH <146373396+KidiIT@users.noreply.github.com>
2024-01-06 03:46:02 +0100

    updated  entry.js

--- entry.js
+++ entry.js
@@ -31,7 +31,6 @@ entryForm.onsubmit=(e)=>{
         formErr.contactErr = 'this field is requried';
 
     }else{
-       console.log({username,password,aspiring});
        alert("Congratulation you've successfully submitted your application,Thanks for time your "+username);
 
     }
